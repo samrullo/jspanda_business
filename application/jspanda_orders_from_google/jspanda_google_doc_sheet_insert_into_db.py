798f8ff161d4ffd683c9cc886b8fe149e9895138
samrullo <amrulloev.subhon@gmail.com>
2020-01-05 21:26:48 +0900

    added jspanda_stats module

--- application/jspanda_orders_from_google/jspanda_google_doc_sheet_insert_into_db.py
+++ application/jspanda_orders_from_google/jspanda_google_doc_sheet_insert_into_db.py
@@ -12,9 +12,9 @@ logger = get_logger()
 engine = create_engine('mysql://samrullo:18Aranid@jspandabusiness.crrz64gldft9.ap-south-1.rds.amazonaws.com/jspanda?charset=utf8')
 
 gdoc_name = "Japansweetproject_new.xlsx"
-gdoc_sheet_name = "Заказы 241219"
+gdoc_sheet_name = "Заказы 060120"
 
-adate = datetime.date(2019, 12, 24)
+adate = datetime.date(2020, 1, 6)
 
 g_docs_obj = GoogleSpreadsheetToDataframe()
 raw_df = g_docs_obj.get_worksheet_as_dataframe(gdoc_name, gdoc_sheet_name)
