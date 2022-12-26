e1e0f3383a676c4e3cb5c4bab11f3690004baec9
samrullo <amrulloev.subhon@gmail.com>
2022-12-26 12:33:33 +0900

    daily spending form

--- application/daily_spending/forms.py
+++ application/daily_spending/forms.py
@@ -6,18 +6,18 @@ import datetime
 from application import images
 
 class SpendingForm(FlaskForm):
-    spent_at=DateTimeField("Spent At",default=datetime.datetime.now)
-    name=StringField("Spending Name")
-    amount = IntegerField("Spent Amount")
-    spending_category=SelectField("Spending Category",coerce=int,validate_choice=False)
-    payment_method=SelectField("Payment Method", coerce=int,validate_choice=False)
-    receipt_image=FileField("Reciept Image", validators=[FileAllowed(images,'Images only are allowed (optional) (.jpg, .jpe, .jpeg, .png, .gif, .svg, and .bmp)!')])
-    submit = SubmitField("Submit")
+    spent_at=DateTimeField("Spent At",default=datetime.datetime.now,render_kw={"class":"form-control"})
+    name=StringField("Spending Name",render_kw={"class":"form-control"})
+    amount = IntegerField("Spent Amount",render_kw={"class":"form-control"})
+    spending_category=SelectField("Spending Category",coerce=int,validate_choice=False,render_kw={"class":"form-control"})
+    payment_method=SelectField("Payment Method", coerce=int,validate_choice=False,render_kw={"class":"form-control"})
+    receipt_image=FileField("Reciept Image", validators=[FileAllowed(images,'Images only are allowed (optional) (.jpg, .jpe, .jpeg, .png, .gif, .svg, and .bmp)!')],render_kw={"class":"form-control-file"})
+    submit = SubmitField("Submit",render_kw={"class":"btn btn-dark"})
 
 class SpendingCategoryForm(FlaskForm):
-    name = StringField("Category Name")
-    submit = SubmitField("Submit")
+    name = StringField("Category Name",render_kw={"class":"form-control"})
+    submit = SubmitField("Submit",render_kw={"class":"btn btn-dark"})
 
 class PaymentMethodForm(FlaskForm):
-    name = StringField("Payment Method")
-    submit = SubmitField("Submit")
+    name = StringField("Payment Method",render_kw={"class":"form-control"})
+    submit = SubmitField("Submit",render_kw={"class":"btn btn-dark"})
