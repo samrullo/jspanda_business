6e2f0041ab2249b760117827d86566ef56fb3c1e
samrullo <amrulloev.subhon@gmail.com>
2023-01-03 19:22:10 +0900

    mortgage app

--- application/mortgage_app/views.py
+++ application/mortgage_app/views.py
@@ -4,7 +4,5 @@ from flask import render_template,current_app
 from flask import send_file
 
 @mortgage_app_bp.route("/")
-def mortgage_app():
-    react_mortgage_app_index_path = current_app.config["REACT_MORTGAGE_APP_FOLDER"]/"build"/"index.html"
-    current_app.logger.info(f"absolute path of '../../../react-mortgage-app/build/index.html' is {os.path.abspath('react-mortgage-app/build/index.html')}")
+def mortgage_app():    
     return render_template("mortgage_index.html")
